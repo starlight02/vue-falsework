7d92eb1f34dd99a0681195995ddddad8c1c197d7
FETE <liuweiqiang@yinda2009.com>
2019-07-02 14:40:09 +0800

    feat: 添加移动端适配方案

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     runtimeCompiler: true,
+    productionSourceMap: false,
+    // configureWebpack: config => {
+    //     if (process.env.NODE_ENV === 'production') {
+    //         // 为生产环境修改配置...
+    //         return {}
+    //     } else {
+    //         // 为开发环境修改配置...
+    //         return {}
+    //     }
+    // }
 };
