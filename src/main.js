61484b01de9ff737318e57f3043558ecdd5f896c
FETE <liuweiqiang@yinda2009.com>
2019-07-22 10:11:16 +0800

    feat: 移除 service worker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import api from './api';
-// import './registerServiceWorker';
 // import '@babel/polyfill';
 import np from 'nprogress';
 import 'nprogress/nprogress.css';
